97e9f474447f6c784535d8392c47be038ad8eea6
Fatimah2014 <80806004+Fatimah2014@users.noreply.github.com>
2021-04-30 22:53:46 -0400

    update index.html , script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,8 +2,34 @@
 
 $ ("#currentDay").text(moment().format("dddd, MMM Do, YYYY"));
 
-console.log('#currentDay')
+console.log(currentDay)
 
-//var today= moment();
 
-//$ ("currentDay").text(today.format("MMM Do, YYYY"));
+var scheduleTime= moment().format('h');
+console.log(scheduleTime);
+const colors = document. getElementsByClassName("col-9");
+
+
+
+
+
+$(".save").on("click", function(e)  {
+    var idvalue = $(this).attr('id')
+    var comment = $(this).siblings(".col-9").val()
+    localStorage.setItem(idvalue, comment)
+});
+
+
+
+const LS = JSON.parse(localStorage.scheduler || '{}'); 
+
+
+
+ var currentTime= moment().format('h');
+
+ var pastTime= moment().subtract(1, 'hour').calender();
+ var futureTime= futureTime>currentTime;
+
+console.log (currentTime)
+console.log(pastTime)
+console.log
