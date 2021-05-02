e1cb3041cac7514a846ce8fb2d1f394fd05ce400
Fatimah2014 <80806004+Fatimah2014@users.noreply.github.com>
2021-05-01 22:34:37 -0400

    updated script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,26 +10,29 @@ console.log(scheduleTime);
 //const colors = document. getElementsByClassName("col-9");
 
 
-$(document).ready(function () {
-    $(".col-1").click(function () {
-       $('.col-9').attr('disabled', true);
 
-    });
-    $(".col-1").dblclick(function () {
+$(document).ready(function() {
+  $(".col-1").click(function () {
+    var theinput = $(this).parent().children('input')
+    console.log('this!!! ', theinput)
+   $(theinput).attr('disabled', true);
+
+
+    $('.col-1').dblclick(function() {
       $('.col-9').removeAttr('disabled');
-      var idvalue = $(this).attr('id')
-      var comment = $(this).siblings(".col-9").val()
-      for (var i = 0, len = buttons.length; i < len; i++) {
-        comment[i].onclick = function (){
-            myFunction (this);
-      localStorage.setItem(idvalue, comment)
-    
-      
+      var theinput = $(this).parent().children('input')
+      console.log('this!!!', theinput)
+
+        idvalue = $(this).attr('id')
+    var comment = $(this).siblings(".col-9").val()
+
+    })
+
  });
-});
+})
 
-var saveButton = document.getElementsByTagName('button');
 
-    }
-}
+
+    
+
 
