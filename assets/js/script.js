ccb3e21a6669a726d883963dc1a360fe1f8e50d6
Fatimah2014 <80806004+Fatimah2014@users.noreply.github.com>
2021-05-03 16:00:07 -0400

    update notes in sript.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,7 +9,8 @@ var scheduleTime= moment().format('h');
 console.log(scheduleTime);
 //when I click on the button the text will be locked.
 //when I double click the text will unlock 
-
+// when refresh input will not delete//
+//all input sent to local storage 
 var button= document.getElementsByClassName("col-1")
 
 $(document).ready(function() {
@@ -32,7 +33,7 @@ $(document).ready(function() {
 
  });
 })//create a loop that set a color and time based off past present future
-
+// added local stroge
 var scheduleTimeM= moment().format('H');
 
   for (var i = 0; i < 9; i++) {   
